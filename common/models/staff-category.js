9f60e6c8159536b4d02693fd6672ed98fbe9c0fe
samiul_alim <msashawon@gmail.com>
2019-10-17 10:46:11 +0600

    update to docker config and staff-category

--- common/models/staff-category.js
+++ common/models/staff-category.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = function (StaffCategory) {
-    StaffCategory.beforeRemote('create', function (next, model) {
-        model.createdAt = Date.now();
-        next();
-    });
+    // StaffCategory.beforeRemote('create', function (next, model) {
+    //     model.createdAt = Date.now();
+    //     next();
+    // });
 
-    StaffCategory.beforeRemote('update', function (next, model) {
-        model.updatedAt = Date.now();
-        next();
-    });
+    // StaffCategory.beforeRemote('update', function (next, model) {
+    //     model.updatedAt = Date.now();
+    //     next();
+    // });
 
 };
