09b525a0b43e4d8c240dd91950f21bd4281987c3
samiul_alim <msashawon@gmail.com>
2020-02-06 16:15:39 +0600

    certification relation with candidate

--- common/models/certification.js
+++ common/models/certification.js
@@ -18,6 +18,42 @@ module.exports = function (Certification) {
         next();
     });
 
+    // Certification.observe('after save', function (ctx, next) {
+    //     if (ctx.isNewInstance) {
+
+    //         Certification.remoteMethod('createApproval', {
+    //             accepts: [{ arg: 'certificationId', type: 'string' }, { arg: 'staffId', type: 'string' }, {
+    //                 arg: 'instituteId',
+    //                 type: 'string'
+    //             },
+    //             { arg: 'candidates', type: 'array' }],
+    //             returns: { arg: 'data', type: 'array', root: true },
+    //             http: { path: 'Institutes/'+ctx.instance.instituteId+'/Certifications', verb: 'post' }
+    //         });
+
+
+    //         // Staffcertification.app.models.create()
+    //         let dataToPost = {
+    //             certificationId: ctx.instance.id,
+    //             staffId: ctx.options.accessToken.userId,
+    //             instituteId: ctx.instance.instituteId,
+    //         }
+    //         console.log(dataToPost)
+    //         let createApproval =Certification.app.models.Staffcertification.create({
+    //             certificationId: ctx.instance.id,
+    //             staffId: ctx.options.accessToken.userId,
+    //             instituteId: ctx.instance.instituteId
+    //         });
+    //         if (createApproval) {
+    //             next()
+    //         }
+            
+    //     }
+    // });
+
+    // Certification.createApproval = async (certificationId, staffId, instituteId) => {
+
+    // }
 
     Certification.addCandidates = async (certification, staffId, instituteId, candidates) => {
 
@@ -49,12 +85,11 @@ module.exports = function (Certification) {
                         });
 
 
-                        let approval = await Certification.app.models.Approval.findById(savedApproval.id, {include: [{certificate: 'candidate'}]});
+                        let approval = await Certification.app.models.Approval.findById(savedApproval.id, { include: [{ certificate: 'candidate' }] });
 
                         console.log('generated approval', approval);
 
                         result.push(approval);
-
 
                         if (!approval) {
                             allOK = false;
@@ -63,11 +98,9 @@ module.exports = function (Certification) {
                         allOK = false;
                     }
 
-
                 } else {
                     allOK = false;
                 }
-
             }
 
             console.log('result', result);
@@ -75,14 +108,13 @@ module.exports = function (Certification) {
             if (allOK) {
                 console.log('A Okay');
 
-
                 return result;
             } else {
                 console.log('all was not ok');
                 return 'An unexpected server error occurred while importing candidates';
             }
         } catch
-            (e) {
+        (e) {
             console.log('Exception in certification', e);
             return e;
         }
@@ -91,13 +123,13 @@ module.exports = function (Certification) {
     };
 
     Certification.remoteMethod('addCandidates', {
-        accepts: [{arg: 'certification', type: 'string'}, {arg: 'staffId', type: 'string'}, {
+        accepts: [{ arg: 'certification', type: 'string' }, { arg: 'staffId', type: 'string' }, {
             arg: 'instituteId',
             type: 'string'
         },
-            {arg: 'candidates', type: 'array'}],
-        returns: {arg: 'data', type: 'array', root: true},
-        http: {path: '/add-candidates', verb: 'post'}
+        { arg: 'candidates', type: 'array' }],
+        returns: { arg: 'data', type: 'array', root: true },
+        http: { path: '/add-candidates', verb: 'post' }
     });
 
 
@@ -107,27 +139,27 @@ module.exports = function (Certification) {
         let result = [];
         try {
 
-            let certifications = await ReturnWithResolvedPromise(await Certification.find({where: {instituteId: instituteId}}));
+            let certifications = await ReturnWithResolvedPromise(await Certification.find({ where: { instituteId: instituteId } }));
             for (const certification of certifications) {
 
                 certification['total'] = await ReturnWithResolvedPromise(await Certification.app.models.Certificate.count(
-                    {certificationId: certification.id}));
+                    { certificationId: certification.id }));
                 certification['approved'] = await ReturnWithResolvedPromise(await Certification.app.models.Certificate.count(
-                    {certificationId: certification.id, isApproved: true}));
+                    { certificationId: certification.id, isApproved: true }));
                 console.log('approved', certification['approved']);
                 certification['pendingApproval'] = await ReturnWithResolvedPromise(await Certification.app.models.Certificate.count(
-                    {and: [{certificationId: certification.id}, {or: [{isApproved: false}, {isApproved: null}]}]}));
+                    { and: [{ certificationId: certification.id }, { or: [{ isApproved: false }, { isApproved: null }] }] }));
                 console.log('pendingApproval', certification['pendingApproval']);
 
                 certification['printed'] = await ReturnWithResolvedPromise(await Certification.app.models.Certificate.count(
-                    {and: [{certificationId: certification.id}, {isPrinted: true}]}));
+                    { and: [{ certificationId: certification.id }, { isPrinted: true }] }));
                 console.log('printed', certification['printed']);
 
 
             }
             return certifications;
         } catch
-            (e) {
+        (e) {
             console.log('Exception in certification', e);
             return e;
         }
@@ -135,8 +167,8 @@ module.exports = function (Certification) {
 
     };
     Certification.remoteMethod('dashboardCertification', {
-        accepts: [{arg: 'institute', type: 'string'}],
-        returns: {arg: 'data', type: 'array', root: true},
-        http: {path: '/certification-metrics', verb: 'get'}
+        accepts: [{ arg: 'institute', type: 'string' }],
+        returns: { arg: 'data', type: 'array', root: true },
+        http: { path: '/certification-metrics', verb: 'get' }
     });
 };
