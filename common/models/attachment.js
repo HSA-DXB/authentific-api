cfdcff9c6961a3feb6c545daf2544964157ad1cc
samiul_alim <msashawon@gmail.com>
2020-02-10 17:48:19 +0600

    candidate image

--- common/models/attachment.js
+++ common/models/attachment.js
@@ -11,30 +11,31 @@ module.exports = function (Attachment) {
         console.log('container type', ctx.args.container.toString().split('-')[2]);
 
 
-        if (ctx.args.container.toString().split('-')[2].includes('candidates')) {
-            for (var file in  response.result.files) {
+        // if (ctx.args.container.toString().split('-')[2].includes('candidates')) {
+        //     for (var file in  response.result.files) {
+        //         console.log()
 
-                let fileName = response.result.files[file][0].name;
-
-
-                let rollNumber = fileName.split('.')[0];
-                console.log('roll number', rollNumber)
-                let candidate = await Attachment.app.models.Candidate.findOne(
-                    {where: {rollNumber: rollNumber}});
+        //         let fileName = response.result.files[file][0].name;
 
 
-                console.log('candidate', candidate)
-
-                candidate.dp=fileName;
-                let updatedCandidate = await Attachment.app.models.Candidate.upsert(
-                    candidate);
-                console.log('updated Candidate', updatedCandidate)
+        //         let rollNumber = fileName.split('.')[0];
+        //         console.log('roll number', rollNumber)
+        //         let candidate = await Attachment.app.models.Candidate.findOne(
+        //             {where: {rollNumber: rollNumber}});
 
 
-            }
+        //         console.log('candidate', candidate)
+
+        //         candidate.dp=fileName;
+        //         let updatedCandidate = await Attachment.app.models.Candidate.upsert(
+        //             candidate);
+        //         console.log('updated Candidate', updatedCandidate)
 
 
-        }
+        //     }
+
+
+        // }
 
 
         return;
