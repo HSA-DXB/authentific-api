ff6a89427e90cca73c8046e8b4e88dd6d7c7047b
samiul_alim <msashawon@gmail.com>
2022-09-13 15:17:50 +0600

    register in stytch complete

--- common/models/staff.js
+++ common/models/staff.js
@@ -1,18 +1,28 @@
-'use strict';
-const VerifyYubiKey = require('../VerifyYubikey');
-const ObjectId = require('mongodb');
-require('dotenv').config();
+"use strict";
+const VerifyYubiKey = require("../VerifyYubikey");
+const ObjectId = require("mongodb");
+require("dotenv").config();
+const stytch = require("stytch");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 module.exports = function (Staff) {
-  Staff.afterRemote('confirm', function (context, client, next) {
+  const client = new stytch.Client({
+    project_id: process.env.STYTCH_PROJECT_ID,
+    secret: process.env.STYTCH_PROJECT_SECRET,
+    env: stytch.envs.test,
+  });
+  Staff.afterRemote("confirm", function (context, client, next) {
     const res = context.res;
-    res.redirect('http://google.be');
+    res.redirect("http://google.be");
   });
 
-
-  Staff.observe('before save', function (ctx, next) {
+  Staff.observe("before save", async function (ctx, next) {
+    async function stytchRegisterUser() {
+      return client.users.create({
+        email: ctx.instance.email,
+      });
+    }
     // console.log(ctx.instance)
     // if(ctx.instance.type=='superadmin'){
     //     let err = new Error();
@@ -26,10 +36,9 @@ module.exports = function (Staff) {
 
       if (JSON.stringify(columnId) != JSON.stringify(accessId)) {
         Staff.findById(accessId, function (err, post) {
-
-          if (post.type == 'user') {
+          if (post.type == "user") {
             let err = new Error();
-            err.message = 'Unauthorized';
+            err.message = "Unauthorized";
             err.status = 401;
             return next(err);
           } else {
@@ -40,37 +49,33 @@ module.exports = function (Staff) {
     } else {
       let accessId = ctx.options.accessToken.userId;
       Staff.findById(accessId, function (err, post) {
-
-        if (post.type == 'user') {
+        if (post.type == "user") {
           let err = new Error();
-          err.message = 'Unauthorized';
+          err.message = "Unauthorized";
           err.status = 401;
           return next(err);
         }
       });
     }
 
-
     if (!ctx.data || !ctx.data.requireYubikey) {
+      const res = await stytchRegisterUser();
+      ctx.instance.StytchData = res.user;
       return next();
-
     }
 
     try {
-
-      console.log('===================');
+      console.log("===================");
       console.log(ctx.data);
       console.log(ctx.data.yubikeyId);
 
-      const yub = require('yub');
+      const yub = require("yub");
 
-
-      yub.init('41713', 'NR+uycIuvGoA1Wh/VmF2eGx2CqQ=');
-
+      yub.init("41713", "NR+uycIuvGoA1Wh/VmF2eGx2CqQ=");
 
       yub.verify(ctx.data.yubikeyId, function (err, response) {
-        console.log('Yubi err', err);
-        console.log('YubiResponse', response.identity);
+        console.log("Yubi err", err);
+        console.log("YubiResponse", response.identity);
 
         if (response && response.valid) {
           console.log(response.identity);
@@ -78,26 +83,16 @@ module.exports = function (Staff) {
           ctx.data.yubikeyId = response.identity;
           next();
         } else {
-
-          return next(new Error('Problem with Yubikey, Please Try Again.'));
-
+          return next(new Error("Problem with Yubikey, Please Try Again."));
         }
       });
-
-
     } catch (e) {
       return next(new Error(e));
-
-
     }
-
-
   });
 
-  Staff.afterRemote('findById', function (context, model, next) {
-
-
-    model.yubikeyId = '';
+  Staff.afterRemote("findById", function (context, model, next) {
+    model.yubikeyId = "";
     next();
     // if (!model.type.toString().includes('client')) {
     //
@@ -119,48 +114,44 @@ module.exports = function (Staff) {
     //     next();
     //
     // }
-
-
   });
 
   // unused func
   function addInstituteToStaff(staff, context, next) {
-
-
     // console.log('staff passed')
-    Staff.app.models.Institute.findById(staff.instituteId, function (err, institute) {
-      // console.log('institute found', institute)
-      if (err) {
-        console.log('err', err);
-        return next(err);
-      }
-      if (!institute) {
-        return next(new Error('could not find institute'));
-      }
+    Staff.app.models.Institute.findById(
+      staff.instituteId,
+      function (err, institute) {
+        // console.log('institute found', institute)
+        if (err) {
+          console.log("err", err);
+          return next(err);
+        }
+        if (!institute) {
+          return next(new Error("could not find institute"));
+        }
 
-      staff['institute'] = institute;
-      delete staff['password'];
-      // console.log('yellow', staff)
-      context.res.status(200).send(staff);
-
-    });
+        staff["institute"] = institute;
+        delete staff["password"];
+        // console.log('yellow', staff)
+        context.res.status(200).send(staff);
+      }
+    );
   }
 
   function cloneFunction(source) {
-    if (Object.prototype.toString.call(source) === '[object Array]') {
+    if (Object.prototype.toString.call(source) === "[object Array]") {
       const clone = [];
       for (let i = 0; i < source.length; i++) {
         clone[i] = cloneFunction(source[i]);
       }
       return clone;
-    } else if (typeof (source) === 'object') {
+    } else if (typeof source === "object") {
       const clone = {};
       for (const prop in source) {
         if (source.hasOwnProperty(prop)) {
-
           if (source.__data) {
             clone[prop] = cloneFunction(source[prop].__data);
-
           } else {
             clone[prop] = cloneFunction(source[prop]);
           }
@@ -174,32 +165,35 @@ module.exports = function (Staff) {
 
   const sendAVerificationToken = (staff) => {
     if (staff.require2FA !== undefined && staff.require2FA) {
-      console.log("========i see=======")
+      console.log("========i see=======");
       if (staff.verified2FA !== undefined && !staff.verified2FA) {
-        console.log("========i see 2=======", accountSid, authToken)
-        const client = require('twilio')(accountSid, authToken);
-        console.log("========going=======")
-        client.verify.services('VA7c8ee0f552059a57254012561686786d')
-          .verifications
-          .create({to: staff.numberToUseIn2FA, channel: 'sms'})
-          .then(verification => {
-            console.log("========verification=======")
-            console.log(verification)
-            console.log("========verification=======")
+        console.log("========i see 2=======", accountSid, authToken);
+        const client = require("twilio")(accountSid, authToken);
+        console.log("========going=======");
+        client.verify
+          .services("VA7c8ee0f552059a57254012561686786d")
+          .verifications.create({ to: staff.numberToUseIn2FA, channel: "sms" })
+          .then((verification) => {
+            console.log("========verification=======");
+            console.log(verification);
+            console.log("========verification=======");
             // response return
-            return verification
+            return verification;
           });
       }
     }
-  }
+  };
 
-  Staff.afterRemote('login', async function (context, token, next) {
-    // console.log('hello world')
+  Staff.afterRemote("login", async function (context, token, next) {
+    console.log(context.args.credentials);
+    let err = new Error();
+    err.message = "Yubikey Missing in Auth.";
+    err.status = 401;
+    return next(err);
     try {
-      const yub = require('yub');
+      const yub = require("yub");
 
-
-      yub.init('41713', 'NR+uycIuvGoA1Wh/VmF2eGx2CqQ=');
+      yub.init("41713", "NR+uycIuvGoA1Wh/VmF2eGx2CqQ=");
 
       let staff = await Staff.findById(token.userId);
       console.log(staff);
@@ -215,113 +209,88 @@ module.exports = function (Staff) {
       2FA functionality ends
       */
 
-
       if (staff.requireYubikey) {
-
         if (!context.args.credentials.yubikey) {
-
           // console.log('Yubikey Missing')
           let err = new Error();
-          err.message = 'Yubikey Missing in Auth.';
+          err.message = "Yubikey Missing in Auth.";
           err.status = 401;
           return next(err);
-
-
         }
 
         let response = await VerifyYubiKey(context.args.credentials.yubikey);
 
         // console.log('response from Ubi API', response)
-        if (response && response.valid && response.identity === staff.yubikeyId) {
-
+        if (
+          response &&
+          response.valid &&
+          response.identity === staff.yubikeyId
+        ) {
           // console.log('Verified')
           return;
         } else {
-
           // console.log('Invalid Yubikey');
 
           let err = new Error();
-          err.message = 'Invalid Yubikey, Try Again.';
+          err.message = "Invalid Yubikey, Try Again.";
           err.status = 401;
           return next(err);
-
         }
-
-
       } else {
-        console.log('yubikey wasn\'t required');
+        console.log("yubikey wasn't required");
         return;
-
       }
-
-
     } catch (e) {
-      console.log("printtttttttttttttttttt")
-      console.log(e)
-      return next(new Error(e));
-    }
-
-
-  });
-
-
-  Staff.afterRemote('admin_login', async function (context, token, next) {
-    // console.log('hello world')
-    try {
-      const yub = require('yub');
-
-
-      yub.init('41713', 'NR+uycIuvGoA1Wh/VmF2eGx2CqQ=');
-
-      let staff = await Staff.findById(token.userId);
-
-
-      if (staff.requireYubikey) {
-
-        if (!context.args.credentials.yubikey) {
-
-          console.log('Yubikey Missing');
-          let err = new Error();
-          err.message = 'Yubikey Missing in Auth.';
-          err.status = 401;
-          return next(err);
-
-
-        }
-
-        let response = await VerifyYubiKey(context.args.credentials.yubikey);
-
-        // console.log('response from Ubi API', response)
-        if (response && response.valid && response.identity === staff.yubikeyId) {
-
-          // console.log('Verified')
-          return;
-        } else {
-
-          // console.log('Invalid Yubikey');
-
-          let err = new Error();
-          err.message = 'Invalid Yubikey, Try Again.';
-          err.status = 401;
-          return next(err);
-
-        }
-
-
-      } else {
-        console.log('yubikey wasn\'t required');
-        return;
-
-      }
-
-
-    } catch (e) {
-
+      console.log("printtttttttttttttttttt");
       console.log(e);
       return next(new Error(e));
     }
+  });
 
+  Staff.afterRemote("admin_login", async function (context, token, next) {
+    // console.log('hello world')
+    try {
+      const yub = require("yub");
 
+      yub.init("41713", "NR+uycIuvGoA1Wh/VmF2eGx2CqQ=");
+
+      let staff = await Staff.findById(token.userId);
+
+      if (staff.requireYubikey) {
+        if (!context.args.credentials.yubikey) {
+          console.log("Yubikey Missing");
+          let err = new Error();
+          err.message = "Yubikey Missing in Auth.";
+          err.status = 401;
+          return next(err);
+        }
+
+        let response = await VerifyYubiKey(context.args.credentials.yubikey);
+
+        // console.log('response from Ubi API', response)
+        if (
+          response &&
+          response.valid &&
+          response.identity === staff.yubikeyId
+        ) {
+          // console.log('Verified')
+          return;
+        } else {
+          // console.log('Invalid Yubikey');
+
+          let err = new Error();
+          err.message = "Invalid Yubikey, Try Again.";
+          err.status = 401;
+          return next(err);
+        }
+      } else {
+        console.log("yubikey wasn't required");
+        return;
+      }
+    } catch (e) {
+      console.log(e);
+      return next(new Error(e));
+    }
   });
 
   // Staff.login = async function (credentials, include, callback) {
@@ -384,6 +353,4 @@ module.exports = function (Staff) {
   //         }
   //     ]
   // });
-
-
 };
